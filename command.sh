4dfa4ed0fdba2ba00d10c68ed341b3022b519c9a
haltaro <mail.to.haltaro@gmail.com>
2017-07-03 19:02:26 -0400

    Revise decoding_layer and senense_to_seq

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/VYiq9WNT4ZC2TuMDfhSfH8' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/NCqDqezHFurHzizuSC6ao5' --NotebookApp.token='' --NotebookApp.allow_origin='*'
